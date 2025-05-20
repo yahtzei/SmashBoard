a49936e299525c427a5aae0410b72a86b5edaf48
Nick <nick.short@e-days.com>
2025-05-20 12:28:54 +0100

    sming's new playground

--- src/smashboard-components.js
+++ src/smashboard-components.js
@@ -1,5 +1,6 @@
 import Mixin from './mixin'
 import SmashBoard from './components/Smashboard.vue';
+import Sming from './components/Sming.vue';
 import Button from './components/Button.vue';
 import Icon from './components/Icon.vue';
 import RosterModal from './components/RosterModal.vue';
@@ -10,6 +11,7 @@ import Roster from './components/Roster.vue';
 if (typeof Vue !== 'undefined') {
   Vue.mixin(Mixin);
   Vue.component('v-smashboard', SmashBoard);
+  Vue.component('v-sming', Sming);
   Vue.component('v-button', Button);
   Vue.component('v-icon', Icon);
   Vue.component('v-roster-modal', RosterModal);
